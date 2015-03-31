ae87185eaf96da60820f80ea1f16af5ef67d39aa
Kurt Henthorn <kurthenthorn@Kurt-Henthorns-Mac-mini.local>
2015-03-31 17:14:39 -0400

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Kurt Henthorn on 3/31/15.
+//  Copyright (c) 2015 Kurt Henthorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Kurt Henthorn on 3/31/15.
+//  Copyright (c) 2015 Kurt Henthorn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
