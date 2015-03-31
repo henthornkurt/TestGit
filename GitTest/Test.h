9e53e5e966c766e5891825068f255bde4f5d4493
Kurt Henthorn <henthorn.kurt@gmail.com>
2015-03-31 17:56:55 -0400

    added a test class

---
+++ GitTest/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitTest
+//
+//  Created by Kurt Henthorn on 3/31/15.
+//  Copyright (c) 2015 Kurt Henthorn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ GitTest/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitTest
+//
+//  Created by Kurt Henthorn on 3/31/15.
+//  Copyright (c) 2015 Kurt Henthorn. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
